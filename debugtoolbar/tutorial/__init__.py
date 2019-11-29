3d961ed0724e882d229c19501fb93401ddf8b619
damian <damian.musiala@gmail.com>
2019-11-29 12:09:58 +0100

    04: Easier Development with debugtoolbar

---
+++ debugtoolbar/tutorial/__init__.py
@@ -0,0 +1,18 @@
+from pyramid.config import Configurator
+from pyramid.response import Response
+
+
+def hello_world(object):
+    """
+    First function of Hello World! in Pyramid.
+    :param object:
+    :return: Response of Hello WOrld!
+    """
+    return xResponse('<body><h1>Hello World!</h1></body>')
+
+
+def main(global_config, **settings):
+    config = Configurator(settings=settings)
+    config.add_route('hello', '/')
+    config.add_view(hello_world, route_name='hello')
+    return config.make_wsgi_app()
